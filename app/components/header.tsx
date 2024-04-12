cf8b9bc11afd7fc332b6ae79efd545f4934034f1
Emanuel Peire <emapeire@outlook.com>
2024-04-12 14:16:08 -0300

    Update styles in header.tsx and add mode toggle component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,18 +4,6 @@ import { ModeToggle } from "./mode-toggle";
 export default function Header() {
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-neutral-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-neutral-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        An AI chat-based interface to learn&nbsp;
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:underline"
-        >
-          <code className="font-mono font-bold">Next.js</code>
-        </a>
-      </p>
-      <ModeToggle />
       <div className="fixed bottom-8 left-0 flex h-32 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <a
           href="https://github.com/nextjsargentina/"
@@ -23,7 +11,6 @@ export default function Header() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span>Next.js Argentina</span>
           <Image
             className="rounded-xl"
             src="/nextjs-dark.svg"
@@ -32,7 +19,23 @@ export default function Header() {
             height={40}
             priority
           />
+          <span>Next.js Argentina</span>
         </a>
+      </div>
+
+      <div className="fixed left-0 top-0 flex w-full justify-center items-center gap-6 border-b border-neutral-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-neutral-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="font-mono">
+          An AI chat-based interface to learn&nbsp;
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            <code className="font-mono font-bold">Next.js</code>
+          </a>
+        </p>
+        <ModeToggle />
       </div>
     </div>
   );
