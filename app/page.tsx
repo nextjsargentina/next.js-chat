51f5658b70a888598dcc475b02f397a99d2d51c0
Emanuel Peire <emapeire@outlook.com>
2024-04-12 17:35:30 -0300

    Update styles in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ChatSection from "./components/chat-section";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-8 lg:py-12 py-24 px-6">
+    <main className="flex min-h-screen flex-col items-center gap-8 lg:py-10 py-20 px-8">
       <Header />
       <ChatSection />
     </main>
