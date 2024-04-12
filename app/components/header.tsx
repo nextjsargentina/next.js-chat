686f407b874e715f0add70ae66c354ea36bfd014
Emanuel Peire <emapeire@outlook.com>
2024-04-12 13:45:16 -0300

    Update styles in header.tsx and chat-avatar.tsx, and remove unused files

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,19 +4,28 @@ export default function Header() {
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-neutral-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-neutral-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Get started by editing&nbsp;
-        <code className="font-mono font-bold">app/page.tsx</code>
-      </p>
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        An AI chat-based interface to learn&nbsp;
         <a
-          href="https://www.llamaindex.ai/"
-          className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
         >
-          <span>Built by LlamaIndex</span>
+          <code className="font-mono font-bold">Next.js</code>
+        </a>
+      </p>
+      <div className="fixed bottom-8 left-0 flex h-32 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <a
+          href="https://github.com/nextjsargentina/"
+          className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>Next.js Argentina</span>
           <Image
             className="rounded-xl"
-            src="/llama.png"
-            alt="Llama Logo"
+            src="/nextjs-dark.svg"
+            alt="NextJS Logo"
             width={40}
             height={40}
             priority
